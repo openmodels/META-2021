5d04b955f95215dd16fc65a8a829351a490ee3ae
ThomasStoerk <71261196+ThomasStoerk@users.noreply.github.com>
2022-11-25 17:49:45 +0100

    Preliminary results export (plus total damage fix)

--- src/Results.jl
+++ src/Results.jl
@@ -1,17 +1,39 @@
-# Load model
-using Mimi
-using MimiMETA
-m = MimiMETA.get_model()
+# Load model and select configuration
+include("../src/MimiMETA.jl")
+model = full_model(; rcp="CP-Base")
 
-///Configure the model - although I believe this is part of the step that loads the model
+# Run the model
+run(model)
+#explore(model) # Launches the Mimi Explorer, which is a graphical interface that lets me look at each computed component. 
 
-*Change parameter values
-set_param!(m, :tcr_transientresponse, 3) #This is still MimiPAGE readme code.
+#= Run with MC
+benchmark = CSV.read("../data/benchmark/ExcelMETA-alltp.csv", DataFrame)
+include("../src/lib/presets.jl")
+for rr in 1:maxrr # rr number of runs
+    println(rr)
+    preset_fill(rr)
+    run(model)
+    # Save results to a data frame
+end
+=#
 
-///Run the model
-run(m)
+# Grab data
+results = getdataframe(model, :TemperatureModel, :T_AT) # Produces initial results data frame
+results[!, :SLR] = model[:SLRModel, :SLR] # Add new result variables
+results[!, :total_damages_global_cumulative] = model[:TotalDamages, :total_damages_global_cumulative] # Add new result variables
+#results[!, :total_damages_cumulative] = model[:TotalDamages, :total_damages_cumulative] # HOW TO ADD AN ENTIRE ARRAY RATHER THAN JUST A SINGLE CAR
 
-///Extract and store variables of interest
-result.m #returns the Model: an array full of different variables. How can I view the Mimi results viewer to decide what I need? (workaround: just look at MimiMETA component .jl files directly)
+#Next problem: how to grab SC-CO2 and SC-CH4 results given that they are scripts outside of the model - need to run them from here I suppose.
 
-marginal_temp = result.mm[:ClimateTemperature, :rt_realizedtemperature] #MimiPAGE code: Mimi seems to organize the array by modules/sub-arrays: find module name, extract the variable of interest.
+# Export data
+using CSV
+CSV.write(raw"C:\Users\Thomas\Dropbox\Tipping points in climate change economics\Mimi testbox\Results\LSE 1 Dec 2022 results.csv", results) #Default is write to working directory
+
+#=
+-SC-CH4 global
+-SC-CO2 global
+-SC-CH4 national
+-SC-CO2 national
+-total damages national
+
+=#
--- src/components/TotalDamages.jl
+++ src/components/TotalDamages.jl
@@ -1,3 +1,4 @@
+using Mimi
 @defcomp TotalDamages begin
 
         # Per capita costs to aggregate
@@ -6,15 +7,15 @@
         market_damages_percap_peryear             = Variable(index = [time, country], unit = "2010 USD PPP") # Market damages per capita per year
 
         # Other parameters
-        population = Parameter(index = [time, country], unit = "million person")
-        #period_length = Parameter(index = [time], unit="year") #Can use 2050 (30 year length) or 2100 (80 year length). However, can I just multiply yearly damages by amount of years? Shouldn't this be a sum, since yearly damages are different for each year?
-
+        population = Parameter(index = [time, country], unit = "inhabitants")
+        
         # Undiscounted costs to calculate
         total_damages_percap_peryear    = Variable(index = [time, country], unit = "2010 USD PPP")  # Undiscounted total damages per capita per year   
-        total_damages_peryear           = Variable(index = [time, country], unit = "Million 2010 USD PPP")  # Undiscounted total damages per year per country
-        total_damages_global_peryear    = Variable(index = [time], unit = "Million 2010 USD PPP")  # Undiscounted total damages per year 
-        total_damages_global_cumulative = Variable(index = [time], unit = "Million 2010 USD PPP") # Undiscounted total damages through 2200
-
+        total_damages_peryear           = Variable(index = [time, country], unit = "2010 USD PPP")  # Undiscounted total damages per year per country
+        total_damages_cumulative        = Variable(index = [time, country], unit = "2010 USD PPP") # Country-level undiscounted total damages through 2200
+        total_damages_global_peryear    = Variable(index = [time], unit = "2010 USD PPP")  # Undiscounted total damages per year 
+        total_damages_global_cumulative = Variable(index = [time], unit = "2010 USD PPP") # Undiscounted total damages through 2200
+        
         function run_timestep(pp, vv, dd, tt)
                 #Define market damages per cap per year
                 for cc in dd.country
@@ -22,7 +23,7 @@
                 end
 
                 # Undiscounted damages
-                vv.total_damages_percap_peryear[tt, :] = pp.market_damages_percap_peryear[t, :] # This is not really needed, but perhaps good in case we add new damages channels at some point.
+                vv.total_damages_percap_peryear[tt, :] = vv.market_damages_percap_peryear[tt, :] # This is not really needed, but perhaps good in case we add new damages channels at some point.
                 vv.total_damages_peryear[tt, :] = vv.total_damages_percap_peryear[tt, :] .* pp.population[tt, :]
                 vv.total_damages_global_peryear[tt] = 0
 
@@ -32,8 +33,14 @@
 
                 if is_first(tt)
                         vv.total_damages_global_cumulative[tt] = vv.total_damages_global_peryear[tt]
+                        for cc in dd.country
+                                vv.total_damages_cumulative[tt, cc] = vv.total_damages_peryear[tt, cc] # Initializes cumulative damages per country
+                        end
                 else
                         vv.total_damages_global_cumulative[tt] = vv.total_damages_global_cumulative[tt-1] + vv.total_damages_global_peryear[tt]
+                        for cc in dd.country
+                                vv.total_damages_cumulative[tt, cc] = vv.total_damages_cumulative[tt-1, cc] + vv.total_damages_peryear[tt,cc] # Need to sum country-level total damages for country-level results
+                        end
                 end
         end
 
