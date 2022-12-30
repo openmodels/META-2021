2afa1296842d1e4abacf41fa2c1d71547b50a910
James Rising <jarising@gmail.com>
2022-12-30 12:45:24 +0200

    fix results bugs

--- src/scch4.jl
+++ src/scch4.jl
@@ -12,12 +12,11 @@ function calculate_scch4(model::Model, pulse_year::Int64, pulse_size::Float64, e
     globalwelfare_marginal = sum(mm[:Utility, :world_disc_utility][pulse_index:191])
 
     global_conspc = sum(mm.base[:Consumption, :conspc][pulse_index, :] .* mm.base[:Utility, :pop][pulse_index, :]) / mm.base[:Utility, :world_population][pulse_index]
-    -(globalwelfare_marginal / (global_conspc^-emuc)) / 1e6 
+    -(globalwelfare_marginal / (global_conspc^-emuc)) / 1e6
 end
 
-include("../src/MimiMETA.jl")
-model = base_model(; rcp="NP-Base", tdamage="pointestimate", slrdamage="mode")
-
+# include("../src/MimiMETA.jl")
+# model = base_model(; rcp="NP-Base", tdamage="pointestimate", slrdamage="mode")
 #calculate_scch4(model, 2020, 0.36, 1.5)#360,000 tCH4 pulse
 
 function calculate_scch4_mc(model::Model, preset_fill::Function, maxrr::Int64, pulse_year::Int64, pulse_size::Float64, emuc::Float64)
@@ -30,8 +29,8 @@ function calculate_scch4_mc(model::Model, preset_fill::Function, maxrr::Int64, p
     scch4s
 end
 
-include("../src/lib/presets.jl")
-benchmark = CSV.read("../data/benchmark/ExcelMETA-alltp.csv", DataFrame)
-model = full_model(; rcp="CP-Base")
-preset_fill(rr) = preset_fill_tp(model, benchmark, rr)
+# include("../src/lib/presets.jl")
+# benchmark = CSV.read("../data/benchmark/ExcelMETA-alltp.csv", DataFrame)
+# model = full_model(; rcp="CP-Base")
+# preset_fill(rr) = preset_fill_tp(model, benchmark, rr)
 #calculate_scch4_mc(model, preset_fill, nrow(benchmark), 2020, 0.24, 1.5) # Runs 500 MC reps.
