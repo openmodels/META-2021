f44418794b97a23daccce3c88c431a950ef7e954
James Rising <jarising@gmail.com>
2023-01-20 23:03:43 -0500

    tests pass

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,4 +20,7 @@ using Test
     include("test_NonMarketDamages.jl")
     include("test_Utility.jl")
     include("test_system_notp.jl")
+    include("test_system_tp.jl")
+    include("test_system_PCFGISISMSAF.jl")
+    include("test_system_rcps.jl")
 end
--- test/test_RCP.jl
+++ test/test_RCP.jl
@@ -5,6 +5,6 @@ include("../src/components/RCP.jl")
 ## Setup the model
 
 model = test_model()
-template = addRCP(model, "NP-Base")
+template = addRCP(model, "RCP8.5")
 
 run(model)
