237f7f9d406d88a11ca4ffe72b0e39a962b20404
James Rising <jarising@gmail.com>
2023-02-24 22:16:35 -0500

    finalize MC system

--- src/montecarlo.jl
+++ src/montecarlo.jl
@@ -4,6 +4,7 @@ using CSV
 using DataFrames
 using LinearAlgebra
 using ProgressMeter
+include("../src/lib/presets.jl")
 
 import Mimi.add_RV!, Mimi.add_save!, Mimi.ReshapedDistribution, Mimi.lhs, Mimi.RandomVariable
 
@@ -136,28 +137,15 @@ function runsim_base(model::Model, draws::DataFrame; save_rvs::Bool=true)
         run(inst)
 
         mcres = Dict{Symbol, Any}()
-        mcres[:TemperatureModel_T_AT] = inst[:TemperatureModel, :T_AT]
-        mcres[:SLRModel_SLR] = inst[:SLRModel, :SLR]
+        mcres[:TemperatureModel_T_AT] = copy(inst[:TemperatureModel, :T_AT])
+        mcres[:SLRModel_SLR] = copy(inst[:SLRModel, :SLR])
         mcres[:Utility_world_disc_utility] = inst[:Utility, :world_disc_utility]
-        mcres[:CO2Model_a0] = inst[:CO2Model, :a0]
-        mcres[:CO2Model_a1] = inst[:CO2Model, :a1]
-        mcres[:CO2Model_a3] = inst[:CO2Model, :a3]
-        mcres[:CO2Model_rho1] = inst[:CO2Model, :rho1]
-        mcres[:CO2Model_rho2] = inst[:CO2Model, :rho2]
-        mcres[:CO2Model_rho3] = inst[:CO2Model, :rho3]
-        mcres[:PostTemperature_r_0] = inst[:PostTemperature, :r_0]
-        mcres[:PostTemperature_r_C] = inst[:PostTemperature, :r_C]
-        mcres[:PostTemperature_r_T] = inst[:PostTemperature, :r_T]
-        mcres[:TemperatureModel_xi_1] = inst[:TemperatureModel, :xi_1]
-        mcres[:TemperatureModel_F_2xCO2] = inst[:TemperatureModel, :F_2xCO2]
-        mcres[:TemperatureModel_fair_ECS] = inst[:TemperatureModel, :fair_ECS]
-        mcres[:TemperatureModel_fair_gamma] = inst[:TemperatureModel, :fair_gamma]
-        mcres[:TemperatureModel_fair_C_0] = inst[:TemperatureModel, :fair_C_0]
-        mcres[:Forcing_F_2xCO2] = inst[:Forcing, :F_2xCO2]
-        mcres[:CH4Model_ch4_alpha] = inst[:CH4Model, :ch4_alpha]
-        mcres[:Consumption_damagepersist] = inst[:Consumption, :damagepersist]
-        mcres[:Utility_EMUC] = inst[:Utility, :EMUC]
-        mcres[:Utility_PRTP] = inst[:Utility, :PRTP]
+
+        if save_rvs
+            for jj in 2:ncol(draws)
+                mcres[Symbol(names(draws)[jj])] = draws[ii, jj]
+            end
+        end
 
         push!(results, mcres)
 
@@ -167,245 +155,176 @@ function runsim_base(model::Model, draws::DataFrame; save_rvs::Bool=true)
     results
 end
 
-function getsim(model::Model, pcf_calib::String, amazon_calib::String, gis_calib::String, wais_calib::String, saf_calib::String, ism_used::Bool, omh_used::Bool, amoc_used::Bool, persist_dist::Bool, emuc_dist::Bool, prtp_dist::Bool; save_rvs::Bool=true)
-    sim = @defsim begin
-
-        # CO2Model
-
-        rv(a0_rv) = make_lognormal(0.021089, 0.13636)
-        rv(a1) = Logistic(0.224, 0.0000339623)
-        rv(a3) = Levy(0.276, 0.00000039629)
-
-        rv(rho1_rv) = make_lognormal(0.0011748, 0.012058)
-        rv(rho2_rv) = Beta(1, 1.6911) # Translate to scaled Kumaraswamy in Shifter
-        rv(rho3_rv) = make_lognormal(0.017462, 0.29837)
-
-        Shifter.a0_rv = a0_rv
-        Shifter.rho1_rv = rho1_rv
-        Shifter.rho2_rv = rho2_rv
-        Shifter.rho3_rv = rho3_rv
-
-        CO2Model_a1 = a1 # CO2Model.a1
-        CO2Model_a3 = a3 # CO2Model.a3
-
-        # PostTemperature
-
-        PostTemperature_r_0 = Normal(32.4, 0.13/1.65*32.4) # PostTemperature.r_0
-        PostTemperature_r_C = Normal(0.019, 0.13/1.65*0.019) # PostTemperature.r_C
-        PostTemperature_r_T = Normal(4.165, 0.13/1.65*4.165) # PostTemperature.r_T
-
-        # Temperature
-
-        rv(xi_1) = Pareto(5.907, 0.11628)
-        rv(F_2xCO2) = Normal(3.45938, 0.43674)
-        rv(fair_ECS) = Normal(3.25312, 0.80031)
-        rv(fair_gamma) = TriangularDist(0.5, 1.23723, 0.5)
-        rv(fair_C_0) = Pareto(1.7062, 53)
-
-        TemperatureModel_xi_1 = xi_1 # TemperatureModel.xi_1
-        TemperatureModel_F_2xCO2 = F_2xCO2 # TemperatureModel.F_2xCO2
-        TemperatureModel_fair_ECS = fair_ECS # TemperatureModel.fair_ECS
-        TemperatureModel_fair_gamma = fair_gamma # TemperatureModel.fair_gamma
-        TemperatureModel_fair_C_0 = fair_C_0 # TemperatureModel.fair_C_0
-
-        # Forcing
-
-        Forcing_F_2xCO2 = F_2xCO2 # Forcing.F_2xCO2
-
-        # CH4
-
-        CH4Model_ch4_alpha = TriangularDist(0.0319967, 0.0400033, 0.036) # CH4Model.ch4_alpha
-
-        # SAF
-
-        SAFModel_ECS = fair_ECS # SAFModel.ECS
-
-        # Damages
-
-        Consumption_seeds[:] = DiscreteUniform(1, typemax(Int64)) # Consumption.seeds
-        Consumption_slruniforms[:] = Uniform(0, 1) # Consumption.slruniforms
-
-        ## sampling(LHSData, corrlist=all_corrlist)
-
-        save(TemperatureModel.T_AT, SLRModel.SLR,
-             Utility.world_disc_utility, CO2Model.a0, CO2Model.a1,
-             CO2Model.a3, CO2Model.rho1, CO2Model.rho2, CO2Model.rho3,
-             PostTemperature.r_0, PostTemperature.r_C,
-             PostTemperature.r_T, TemperatureModel.xi_1,
-             TemperatureModel.F_2xCO2, TemperatureModel.fair_ECS,
-             TemperatureModel.fair_gamma, TemperatureModel.fair_C_0,
-             Forcing.F_2xCO2, CH4Model.ch4_alpha)
-    end
+function getsim(trials::Int64, pcf_calib::String, amazon_calib::String, gis_calib::String, wais_calib::String, saf_calib::String, persist_dist::Bool, emuc_dist::Bool, prtp_dist::Bool)
+    draws = getsim_base(trials, persist_dist, emuc_dist, prtp_dist)
 
     # Interactions
 
     dists = allinteractrates((symbol, ratemu, ratese) -> Normal(ratemu, ratese))
 
-    add_RV!(sim, :Interactions_gis2amoc, dists["gis"]["amoc"])
-    add_RV!(sim, :Interactions_wais2amoc, dists["wais"]["amoc"])
-    add_RV!(sim, :Interactions_amaz2amoc, dists["amaz"]["amoc"])
-    add_RV!(sim, :Interactions_nino2amoc, dists["nino"]["amoc"])
-    add_RV!(sim, :Interactions_amoc2gis, dists["amoc"]["gis"])
-    add_RV!(sim, :Interactions_wais2gis, dists["wais"]["gis"])
-    add_RV!(sim, :Interactions_amaz2gis, dists["amaz"]["gis"])
-    add_RV!(sim, :Interactions_nino2gis, dists["nino"]["gis"])
-    add_RV!(sim, :Interactions_amoc2wais, dists["amoc"]["wais"])
-    add_RV!(sim, :Interactions_gis2wais, dists["gis"]["wais"])
-    add_RV!(sim, :Interactions_amaz2wais, dists["amaz"]["wais"])
-    add_RV!(sim, :Interactions_nino2wais, dists["nino"]["wais"])
-    add_RV!(sim, :Interactions_amoc2amaz, dists["amoc"]["amaz"])
-    add_RV!(sim, :Interactions_gis2amaz, dists["gis"]["amaz"])
-    add_RV!(sim, :Interactions_wais2amaz, dists["wais"]["amaz"])
-    add_RV!(sim, :Interactions_nino2amaz, dists["nino"]["amaz"])
-    add_RV!(sim, :Interactions_amoc2nino, dists["amoc"]["nino"])
-    add_RV!(sim, :Interactions_gis2nino, dists["gis"]["nino"])
-    add_RV!(sim, :Interactions_wais2nino, dists["wais"]["nino"])
-    add_RV!(sim, :Interactions_amaz2nino, dists["amaz"]["nino"])
-
-    if save_rvs
-        add_save!(sim, :Interactions, :gis2amoc)
-        add_save!(sim, :Interactions, :wais2amoc)
-        add_save!(sim, :Interactions, :amaz2amoc)
-        add_save!(sim, :Interactions, :nino2amoc)
-        add_save!(sim, :Interactions, :amoc2gis)
-        add_save!(sim, :Interactions, :wais2gis)
-        add_save!(sim, :Interactions, :amaz2gis)
-        add_save!(sim, :Interactions, :nino2gis)
-        add_save!(sim, :Interactions, :amoc2wais)
-        add_save!(sim, :Interactions, :gis2wais)
-        add_save!(sim, :Interactions, :amaz2wais)
-        add_save!(sim, :Interactions, :nino2wais)
-        add_save!(sim, :Interactions, :amoc2amaz)
-        add_save!(sim, :Interactions, :gis2amaz)
-        add_save!(sim, :Interactions, :wais2amaz)
-        add_save!(sim, :Interactions, :nino2amaz)
-        add_save!(sim, :Interactions, :amoc2nino)
-        add_save!(sim, :Interactions, :gis2nino)
-        add_save!(sim, :Interactions, :wais2nino)
-        add_save!(sim, :Interactions, :amaz2nino)
-    end
-
-    # ISM
-
-    if ism_used
-        # ISMModel_uniforms[:, :] = Uniform(0, 1) # ISMModel.uniforms
-        add_RV!(sim, :ISMModel_uniforms, ReshapedDistribution([dim_count(model, :time), dim_count(model, :monsoonsteps)], Uniform(0, 1)))
-    end
-
-    # OMH
-
-    if omh_used
-        # OMH_uniforms[:] = Uniform(0, 1) # OMH.uniforms
-        add_RV!(sim, :OMH_uniforms, ReshapedDistribution([dim_count(model, :time)], Uniform(0, 1)))
-    end
-
-    # AMOC
-
-    if amoc_used
-        # AMOC_uniforms[:] = Uniform(0, 1) # AMOC.uniforms
-        add_RV!(sim, :AMOC_uniforms, ReshapedDistribution([dim_count(model, :time)], Uniform(0, 1)))
-    end
+    draws.Interactions_gis2amoc = rand(dists["gis"]["amoc"], trials)
+    draws.Interactions_wais2amoc = rand(dists["wais"]["amoc"], trials)
+    draws.Interactions_amaz2amoc = rand(dists["amaz"]["amoc"], trials)
+    draws.Interactions_nino2amoc = rand(dists["nino"]["amoc"], trials)
+    draws.Interactions_amoc2gis = rand(dists["amoc"]["gis"], trials)
+    draws.Interactions_wais2gis = rand(dists["wais"]["gis"], trials)
+    draws.Interactions_amaz2gis = rand(dists["amaz"]["gis"], trials)
+    draws.Interactions_nino2gis = rand(dists["nino"]["gis"], trials)
+    draws.Interactions_amoc2wais = rand(dists["amoc"]["wais"], trials)
+    draws.Interactions_gis2wais = rand(dists["gis"]["wais"], trials)
+    draws.Interactions_amaz2wais = rand(dists["amaz"]["wais"], trials)
+    draws.Interactions_nino2wais = rand(dists["nino"]["wais"], trials)
+    draws.Interactions_amoc2amaz = rand(dists["amoc"]["amaz"], trials)
+    draws.Interactions_gis2amaz = rand(dists["gis"]["amaz"], trials)
+    draws.Interactions_wais2amaz = rand(dists["wais"]["amaz"], trials)
+    draws.Interactions_nino2amaz = rand(dists["nino"]["amaz"], trials)
+    draws.Interactions_amoc2nino = rand(dists["amoc"]["nino"], trials)
+    draws.Interactions_gis2nino = rand(dists["gis"]["nino"], trials)
+    draws.Interactions_wais2nino = rand(dists["wais"]["nino"], trials)
+    draws.Interactions_amaz2nino = rand(dists["amaz"]["nino"], trials)
 
     # Permafrost
 
     if pcf_calib == "Kessler probabilistic"
-        # PCFModel.propCH4 = Normal(0.023, 0.006)
-        # PCFModel.beta_PF = Normal(0.172, 0.0261)
-        # PCFModel.C_PF = Normal(1035, 76.53)
-        # PCFModel.propPassive = Normal(0.4, 0.055)
-        # PCFModel.tau = Normal(70, 30)
-        add_RV!(sim, :PCFModel_propCH4, Normal(0.023, 0.006))
-        add_RV!(sim, :PCFModel_beta_PF, Normal(0.172, 0.0261))
-        add_RV!(sim, :PCFModel_C_PF, Normal(1035, 76.53))
-        add_RV!(sim, :PCFModel_propPassive, Normal(0.4, 0.055))
-        add_RV!(sim, :PCFModel_tau, Normal(70, 30))
-
-        if save_rvs
-            add_save!(sim, :PCFModel, :propCH4)
-            add_save!(sim, :PCFModel, :beta_PF)
-            add_save!(sim, :PCFModel, :C_PF)
-            add_save!(sim, :PCFModel, :propPassive)
-            add_save!(sim, :PCFModel, :tau)
-        end
+        draws.PCFModel_propCH4 = rand(Normal(0.023, 0.006), trials)
+        draws.PCFModel_beta_PF = rand(Normal(0.172, 0.0261), trials)
+        draws.PCFModel_C_PF = rand(Normal(1035, 76.53), trials)
+        draws.PCFModel_propPassive = rand(Normal(0.4, 0.055), trials)
+        draws.PCFModel_tau = rand(Normal(70, 30), trials)
     end
 
     # Amazon
 
     if amazon_calib == "Distribution"
-        # AmazonDieback.Delta_AMAZ = TriangularDist(10, 250, 50)
-        add_RV!(sim, :AmazonDieback_Delta_AMAZ, TriangularDist(10, 250, 50))
-
-        if save_rvs
-            add_save!(sim, :AmazonDieback, :Delta_AMAZ)
-        end
-    end
-    if amazon_calib != "none"
-        # AmazonDieback_uniforms[:] = Uniform(0, 1) # AmazonDieback.uniforms
-        add_RV!(sim, :AmazonDieback_uniforms, ReshapedDistribution([dim_count(model, :time)], Uniform(0, 1)))
+        draws.AmazonDieback_Delta_AMAZ = rand(TriangularDist(10, 250, 50), trials)
     end
 
     # GIS
 
     if gis_calib == "Distribution"
-        # GISModel.avoldot = Normal(-0.0000106, 0.0000244/0.5/100) # Only works with a meltmult of 1
-        add_RV!(sim, :GISModel_avoldot, Normal(-0.0000106, 0.0000244/0.5/100)) # Only works with a meltmult of 1
-
-        if save_rvs
-            add_save!(sim, :GISModel, :avoldot)
-        end
+        draws.GISModel_avoldot = rand(Normal(-0.0000106, 0.0000244/0.5/100), trials) # Only works with a meltmult of 1
     end
 
     # WAIS
 
     if wais_calib == "Distribution"
-        # WAISmodel.waisrate = make_lognormal(3.3 / 1000, 1.65 / 1000)
-        add_RV!(sim, :WAISmodel_waisrate, make_lognormal(3.3 / 1000, 1.65 / 1000))
-        # WAISmodel_uniforms[:] = Uniform(0, 1) # WAISmodel.uniforms
-        add_RV!(sim, :WAISmodel_uniforms, [Uniform(0, 1) for tt in 1:dim_count(model, :time)])
-
-        if save_rvs
-            add_save!(sim, :WAISmodel, :waisrate)
-        end
+        draws.WAISmodel_waisrate = rand(make_lognormal(3.3 / 1000, 1.65 / 1000), trials)
     end
 
     # SAF
 
     if saf_calib == "Distribution"
-        # SAFModel.delta = TriangularDist(-1, 1, 0)
-        # SAFModel.FRT = TriangularDist(10, 55, 20)
-        add_RV!(sim, :SAFModel_saf_delta, TriangularDist(-1, 1, 0))
-        add_RV!(sim, :SAFModel_FRT, TriangularDist(10, 55, 20))
-
-        if save_rvs
-            add_save!(sim, :SAFModel, :saf_delta)
-            add_save!(sim, :SAFModel, :FRT)
-        end
+        draws.SAFModel_saf_delta = rand(TriangularDist(-1, 1, 0), trials)
+        draws.SAFModel_FRT = rand(TriangularDist(10, 55, 20), trials)
     end
 
-    # Damages
-
-    if persist_dist
-        add_RV!(sim, :Consumption_damagepersist, Uniform(0, 1))
-
-        if save_rvs
-            add_save!(sim, :Consumption, :damagepersist)
-        end
-    end
-
-    # Utility
-
-    if emuc_dist
-        add_RV!(sim, :Consumption_EMUC, TriangularDist(0.5, 2, 1.5))
-        if save_rvs
-            add_save!(sim, :Consumption, :EMUC)
-        end
-    end
-    if prtp_dist
-        add_RV!(sim, :Consumption_PRTP, TriangularDist(0.001, 0.02, 0.01))
-        if save_rvs
-            add_save!(sim, :Consumption, :PRTP)
-        end
-    end
-
-    sim
+    draws
 end
+
+function runsim(model::Model, draws::DataFrame, ism_used::Bool, omh_used::Bool, amoc_used::Bool, amazon_calib::String, wais_calib::String; save_rvs::Bool=true)
+    if wais_calib == "Distribution"
+        set_param!(model, :WAISmodel, :waisrate, :WAISmodel_waisrate, 0.0033) # set up global connection
+    end
+
+    inst = Mimi.build(model)
+
+    progress = Progress(nrow(draws), 1)
+
+    results = Dict{Symbol, Any}[]
+    for ii in 1:nrow(draws)
+        for jj in 2:ncol(draws)
+            if has_parameter(model.md, Symbol(names(draws)[jj]))
+                update_param!(inst, Symbol(names(draws)[jj]), draws[ii, jj])
+            end
+        end
+
+        # SAF
+
+        update_param!(inst, :SAFModel_ECS, draws.TemperatureModel_fair_ECS[ii]) # SAFModel.ECS
+
+        # Damages
+
+        update_param!(inst, :Consumption_seeds, rand(DiscreteUniform(1, typemax(Int64)), dim_count(model, :country)))
+        update_param!(inst, :Consumption_slruniforms, rand(Uniform(0, 1), dim_count(model, :country)))
+
+        # ISM
+
+        if ism_used
+            update_param!(inst, :ISMModel_uniforms, rand(Uniform(0, 1), (dim_count(model, :time), dim_count(model, :monsoonsteps))))
+        end
+
+        # OMH
+
+        if omh_used
+            update_param!(inst, :OMH_uniforms, rand(Uniform(0, 1), dim_count(model, :time)))
+        end
+
+        # AMOC
+
+        if amoc_used
+            update_param!(inst, :AMOC_uniforms, rand(Uniform(0, 1), dim_count(model, :time)))
+        end
+
+        # Amazon
+
+        if amazon_calib != "none"
+            update_param!(inst, :AmazonDieback_uniforms, rand(Uniform(0, 1), dim_count(model, :time)))
+        end
+
+        # WAIS
+
+        if wais_calib == "Distribution"
+            update_param!(inst, :WAISmodel_uniforms, rand(Uniform(0, 1), dim_count(model, :time)))
+        end
+
+        run(inst)
+
+        mcres = Dict{Symbol, Any}()
+        mcres[:TemperatureModel_T_AT] = copy(inst[:TemperatureModel, :T_AT])
+        mcres[:SLRModel_SLR] = copy(inst[:SLRModel, :SLR])
+        mcres[:Utility_world_disc_utility] = inst[:Utility, :world_disc_utility]
+
+        if save_rvs
+            for jj in 2:ncol(draws)
+                mcres[Symbol(names(draws)[jj])] = draws[ii, jj]
+            end
+        end
+
+        push!(results, mcres)
+
+        next!(progress)
+    end
+
+    results
+end
+
+function simdataframe(model::Model, results::Vector{Dict{Symbol, Any}}, comp::Symbol, name::Symbol)
+    key = Symbol("$(comp)_$(name)")
+    if results[1][key] isa Number
+        df = DataFrame(trialnum=1:length(results))
+        df[!, name] = [results[ii][key] for ii in 1:length(results)]
+    else
+        dfbase = getdataframe(model, comp, name)
+        df = nothing
+        for ii in 1:length(results)
+            mcdf = dfbase[!, :]
+            mcdf[!, name] = results[ii][key]
+            mcdf.trialnum .= ii
+            if df == nothing
+                df = mcdf
+            else
+                df = vcat(df, mcdf)
+            end
+        end
+    end
+    df
+end
+
+# function myupdate_param!(inst, unique_name::Symbol, value)
+#     if has_parameter(inst.md, unique_name)
+#         update_param!(inst, unique_name, value)
+#     else
+#         parts = split(String(unique_name), "_")
+#         set_param!(inst, Symbol(parts[1]), Symbol(join(parts[2:end], "_")), unique_name, value)
+#     end
+# end
--- test/test_montecarlo_tp.jl
+++ test/test_montecarlo_tp.jl
@@ -4,55 +4,7 @@ include("../src/MimiMETA.jl")
 include("../src/montecarlo.jl")
 include("../src/lib/presets.jl")
 
-do_test = "notp"
-
-if do_test == "full"
-    model = full_model(rcp="RCP4.5", ssp="SSP2")
-    update_param!(model, :CH4Model, :decay_rate, 1 / 12.4)
-    prepare_montecarlo!(model)
-    sim = getsim(model, "Fit of Hope and Schaefer (2016)", # PCF
-                 "Cai et al. central value", # AMAZ
-                 "Nordhaus central value", # GIS
-                 "Distribution", # WAIS
-                 "Distribution", # SAF
-                 true, # ism_used
-                 true, # omh_used
-                 true, # amoc_used
-                 false, # persit
-                 false, # emuc
-                 false) # prtp
-elseif do_test == "some" # PCFGISISMSAF
-    model = full_model(rcp="RCP4.5", ssp="SSP2"; interaction=false, omh=false, amaz=false, wais=false, amoc=false)
-    update_param!(model, :CH4Model, :decay_rate, 1 / 12.4)
-    prepare_montecarlo!(model)
-    sim = getsim(model, "Fit of Hope and Schaefer (2016)", # PCF
-                 "none", # AMAZ
-                 "Nordhaus central value", # GIS
-                 "none", # WAIS
-                 "Distribution", # SAF
-                 true, # ism_used
-                 false, # omh_used
-                 false, # amoc_used
-                 false, # persit
-                 false, # emuc
-                 false) # prtp
-elseif do_test == "notp"
-    model = base_model(rcp="RCP4.5")
-    update_param!(model, :CH4Model, :decay_rate, 1 / 12.4)
-    ## prepare_montecarlo!(model)
-    draws = getsim_base(500, false, false, false)
-    results = runsim_base(model, draws)
-end
-
-si = run(sim, model, 500)
-
-if do_test == "full"
-    benchmark = CSV.read("../data/benchmark/ExcelMETA-alltp.csv", DataFrame)
-else
-    benchmark = CSV.read("../data/benchmark/ExcelMETA-notp.csv", DataFrame)
-end
-
-mapping = Dict{String, Tuple{Symbol, Symbol}}("Nonlinear SAF / Distribution" => (:SAFModel, :delta),
+mapping = Dict{String, Tuple{Symbol, Symbol}}("Nonlinear SAF / Distribution" => (:SAFModel, :saf_delta),
                                               "Warming half-life / Distribution" => (:SAFModel, :FRT),
                                               "xi_1" => (:TemperatureModel, :xi_1),
                                               "F_2XCO2" => (:TemperatureModel, :F_2xCO2),
@@ -61,50 +13,108 @@ mapping = Dict{String, Tuple{Symbol, Symbol}}("Nonlinear SAF / Distribution" => 
                                               "C0" => (:TemperatureModel, :fair_C_0),
                                               "Dataset 1" => (:CH4Model, :ch4_alpha))
 
-tvals = Dict{String, Float64}()
-missings = String[]
-errors = String[]
-for col in names(benchmark)[2:end]
-    println(col)
-    info = split(col, " / ")
-    try
-        if col ∈ ["Global welfare", "World consumption per capita in 2020"]
-            continue
-        elseif col ∈ ["a_0", "a_1", "a_3", "rho_1", "rho_2", "rho_3"]
-            df = getdataframe(si, :CO2Model, Symbol(replace(col, "_" => "")))
-        elseif col ∈ ["r_{0) / Distribution", "r_{C} / Distribution", "r_{T} / Distribution"]
-            df = getdataframe(si, :PostTemperature, Symbol(replace(info[1], "{" => "", "}" => "", ")" => "")))
-        elseif col ∈ keys(mapping)
-            df = getdataframe(si, mapping[col][1], mapping[col][2])
-        elseif info[2] == "Kessler probabilistic" || info[1] ∈ ["Delta_AMAZ", "avoldot0"]
-            continue # not included in test
-        elseif info[2] == "Atmospheric temperature"
-            df = getdataframe(si, :TemperatureModel, :T_AT)
-        elseif info[2] == "SLR total (m)"
-            df = getdataframe(si, :SLRModel, :SLR)
-        elseif info[2] == "World aggregate consumption per capita"
-            continue
-            # df = getdataframe(si, :Utility, :equiv_conspc)
-        elseif info[2] ∈ ["beta1dist", "beta2dist", "errdist"]
-            continue # not saved
-        elseif info[1] == "waisrate"
-            df = getdataframe(si, :WAISmodel, :waisrate)
-            df = DataFrame(:waisrate => df.waisrate * 1000, :trialnum => df.trialnum)
-        else
-            push!(missings, col)
-            continue
-        end
-        if length(info) == 2 && info[2] != "Distribution"
-            vals = convert(Vector{Float64}, df[df.time .== parse(Int64, info[1]), 2])
-        else
-            vals = convert(Vector{Float64}, df[!, 1])
-        end
-        comps = benchmark[!, col]
-
-        result = UnequalVarianceTTest(vals[.!isnan.(vals)], comps)
-        tvals[col] = result.t
-    catch
-        push!(errors, col)
+for do_test in ["notp", "full", "some"]
+    if do_test == "full"
+        global model = full_model(rcp="RCP4.5", ssp="SSP2")
+        update_param!(model, :CH4Model, :decay_rate, 1 / 12.4)
+        draws = getsim(500, "Fit of Hope and Schaefer (2016)", # PCF
+                       "Cai et al. central value", # AMAZ
+                       "Nordhaus central value", # GIS
+                       "Distribution", # WAIS
+                       "Distribution", # SAF
+                       false, # persit
+                       false, # emuc
+                       false) # prtp
+        results = runsim(model, draws, true, # ism_used
+                         true, # omh_used
+                         true, # amoc_used
+                         "Cai et al. central value", # AMAZ
+                         "Distribution") # WAIS
+    elseif do_test == "some" # PCFGISISMSAF
+        global model = full_model(rcp="RCP4.5", ssp="SSP2"; interaction=false, omh=false, amaz=false, wais=false, amoc=false)
+        update_param!(model, :CH4Model, :decay_rate, 1 / 12.4)
+        draws = getsim(500, "Fit of Hope and Schaefer (2016)", # PCF
+                       "none", # AMAZ
+                       "Nordhaus central value", # GIS
+                       "none", # WAIS
+                       "Distribution", # SAF
+                       false, # persit
+                       false, # emuc
+                       false) # prtp
+        results = runsim(model, draws, true, # ism_used
+                         false, # omh_used
+                         false, # amoc_used
+                         "none", # AMAZ
+                         "none") # WAIS
+    elseif do_test == "notp"
+        global model = base_model(rcp="RCP4.5")
+        update_param!(model, :CH4Model, :decay_rate, 1 / 12.4)
+        draws = getsim_base(500, false, false, false)
+        results = runsim_base(model, draws)
     end
-    # @test result.t < 2.56 # 99%
+
+    run(model) # run once for simdataframe
+
+    for compare_same in [true, false]
+        if (compare_same && do_test == "full") || (!compare_same && do_test == "some")
+            benchmark = CSV.read("../data/benchmark/ExcelMETA-alltp.csv", DataFrame)
+        elseif (compare_same && do_test == "some") || (!compare_same && do_test == "notp")
+            benchmark = CSV.read("../data/benchmark/ExcelMETA-PCFGISISMSAF.csv", DataFrame)
+        else
+            benchmark = CSV.read("../data/benchmark/ExcelMETA-notp.csv", DataFrame)
+        end
+
+        tvals = Dict{String, Float64}()
+        missings = String[]
+        errors = String[]
+        for col in names(benchmark)[2:end]
+            info = split(col, " / ")
+            try
+                if col ∈ ["Global welfare", "World consumption per capita in 2020"]
+                    continue
+                elseif col ∈ ["a_0", "a_1", "a_3", "rho_1", "rho_2", "rho_3"]
+                    df = simdataframe(model, results, :CO2Model, Symbol(replace(col, "_" => "")))
+                elseif col ∈ ["r_{0) / Distribution", "r_{C} / Distribution", "r_{T} / Distribution"]
+                    df = simdataframe(model, results, :PostTemperature, Symbol(replace(info[1], "{" => "", "}" => "", ")" => "")))
+                elseif col ∈ keys(mapping)
+                    df = simdataframe(model, results, mapping[col][1], mapping[col][2])
+                elseif info[2] == "Kessler probabilistic" || info[1] ∈ ["Delta_AMAZ", "avoldot0"]
+                    continue # not included in test
+                elseif info[2] == "Atmospheric temperature"
+                    df = simdataframe(model, results, :TemperatureModel, :T_AT)
+                elseif info[2] == "SLR total (m)"
+                    df = simdataframe(model, results, :SLRModel, :SLR)
+                elseif info[2] == "World aggregate consumption per capita"
+                    continue
+                    # df = simdataframe(model, results, :Utility, :equiv_conspc)
+                elseif info[2] ∈ ["beta1dist", "beta2dist", "errdist"]
+                    continue # not saved
+                elseif info[1] == "waisrate"
+                    df = simdataframe(model, results, :WAISmodel, :waisrate)
+                    df = DataFrame(:trialnum => df.trialnum, :waisrate => df.waisrate * 1000)
+                else
+                    push!(missings, col)
+                    continue
+                end
+                if length(info) == 2 && info[2] != "Distribution"
+                    vals = convert(Vector{Float64}, df[df.time .== parse(Int64, info[1]), 2])
+                else
+                    vals = convert(Vector{Float64}, df[!, 2])
+                end
+                comps = benchmark[!, col]
+
+                result = UnequalVarianceTTest(vals[.!isnan.(vals)], comps)
+                tvals[col] = result.t
+                if compare_same
+                    @test abs(result.t) < 2.56 # 99%
+                end
+            catch
+                push!(errors, col)
+            end
+        end
+
+        if !compare_same
+            @test maximum(abs.(values(tvals))) > 2.56
+        end
+    end
 end
